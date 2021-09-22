50457caf52862f37391b9c6a0f37b021768a0509
jchin39 <jchin39@ucmerced.edu>
2021-09-21 20:35:04 -0700

    Lab 2

--- array3.cpp
+++ array3.cpp
@@ -2,9 +2,11 @@
 #include <string>
 using namespace std;
 int main() {
+    // initializing size and counters 
     int x, row = 1, counter = 0;
     cout << "Enter the size of a 2D array: ";
     cin >> x;
+    // check validity of array size
     if (x > 10) {
         cout << "ERROR: your array is too large! Enter 1 to 10." << endl;
         exit(0);
@@ -13,18 +15,22 @@ int main() {
         cout << "ERROR: You entered an incorrect value for the array size" << endl;
         exit(0);
     }
+    // initializing array 
     int Array[x][x];
+    // funneling in array values
     for (int i = 0; i < x; i++) {
         cout << "Enter the values in the array for row "<< row <<", separated by a space, and press enter: ";
         row++;
         for (int j = 0; j < x; j++) {
             cin >> Array[i][j];
+            //checking for negative values
             if (Array[i][j] < 0) {
                 counter++;
             } 
         }
     
     }
+    //conclusion
     if (counter > 0) {
         cout << "There are " << counter << " negative values!" << endl;
     }   
