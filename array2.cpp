30bb6fbd960529269c9b66e7c80770022cba511e
jchin39 <jchin39@ucmerced.edu>
2021-09-21 20:35:51 -0700

    Lab 2

--- array2.cpp
+++ array2.cpp
@@ -6,7 +6,7 @@ int main() {
     string word;
     cout << "Enter the string to reverse: ";
     cin >> word;
-    //Printing out the letters in reverse by integrating the string in reverse, then printing i.
+    //Printing out the letters in reverse by integrating the string in reverse, then printing it.
     for (int i = word.size() -1; i >= 0; i--) {
         cout << word[i];
     }
